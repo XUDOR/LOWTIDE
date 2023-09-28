7cfd3060d2e479ee31863d82086b771f3c905d25
Roderick-Shoolbraid <rdxenv@gmail.com>
2023-09-27 20:53:18 -0700

    tail

---
+++ tail.js
@@ -0,0 +1,17 @@
+// tail function
+const tail = arr => arr.slice(1);
+
+// assertEqual 
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+//test 
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
