1c0ac85bd2c0f57d18ea24d77a12056c3d29d015
Roderick Shoolbraid <106110618+XUDOR@users.noreply.github.com>
2023-10-01 12:47:33 -0700

    Add files via upload

---
+++ eqObjects.js
@@ -0,0 +1,53 @@
+// eqArrays function
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+      return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+          return false;
+      }
+  }
+  return true;
+}
+
+// Function to assert the equality of two values
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+// Function to compare two objects
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (!keys2.includes(key)) {
+      return false;
+    }
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Test cases
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
