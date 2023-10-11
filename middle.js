d0dcf141826ed04eeb51ab27dddaf4a516f9e083
Roderick Shoolbraid <rdxenv@gmail.com>
2023-10-10 22:44:19 -0700

    module / test / update

--- middle.js
+++ middle.js
@@ -1,25 +1,5 @@
-// eqArrays function
-function eqArrays(arr1, arr2) { // function declaration
-  if (arr1.length !== arr2.length) { // array comparison
-      return false;
-  }
-  for (let i = 0; i < arr1.length; i++) { // loops through array
-      if (arr1[i] !== arr2[i]) {
-          return false;
-      }
-  }
-  return true;
-}
-
-
-//assertEquals
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) { // Using eqArrays for array comparison
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`); // console log passed
-  } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`); // console log fail
-  }
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 //middle
 const middle = function(array) { // function expression
@@ -31,7 +11,8 @@ const middle = function(array) { // function expression
   } else { // If odd number of elements
     return [array[Math.floor(length / 2)]];
   }
-}
+};
 
-
+//module export
+module.exports = middle;
 
