185eeaf6e72ed5274fcc00d06b3eefa509297345
Roderick Shoolbraid <rdxenv@gmail.com>
2023-10-10 23:51:04 -0700

    update

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,12 +1,12 @@
 // eqArrays function
 function eqArrays(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-      return false;
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-          return false;
-      }
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
   return true;
 }
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,3 +1,15 @@
+
+
+
+
+
+
+
+
+
+
+
+
 const assertEqual = require('../assertEqual');
 const assertArraysEqual = require('../assertArraysEqual');
 
