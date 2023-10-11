43cc650ead1cea5bc5d8dfabb26944c14fbe464d
Roderick Shoolbraid <rdxenv@gmail.com>
2023-10-10 22:10:14 -0700

    module--lint--test

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,19 @@
+const assertEqual = require('./assertEqual');
+
+
 // eqArrays function
 function eqArrays(arr1, arr2) { // function definition 
-    if (arr1.length !== arr2.length) { // array comparison
-        return false;
+  if (arr1.length !== arr2.length) { // array comparison
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) { // loops through array
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
-    for (let i = 0; i < arr1.length; i++) { // loops through array
-        if (arr1[i] !== arr2[i]) {
-            return false;
-        }
-    }
+  }
     return true;
 }
 
-// assertEqual
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`); // console log fail
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`); // console log passed
-  }
-};
+
+//module export
+module.exports = eqArrays;
