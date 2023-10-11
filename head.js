c467a393f96d0a09575cbb839800bf4aeaf16aa1
Roderick Shoolbraid <rdxenv@gmail.com>
2023-10-10 21:48:13 -0700

    export update

--- head.js
+++ head.js
@@ -3,3 +3,5 @@ const assertEqual = require('./assertEqual');
 
 const head = arr => arr[0];
 
+//module export
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,17 +1,7 @@
+const assertEqual = require('./assertEqual');
+
 // tail function
 const tail = arr => arr.slice(1);
 
-// assertEqual 
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-//test 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+//module export
+module.exports = tail;
